dc63414bff461c0c3799da8eb8cad84384ae68df
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-01 14:49:52 -0600

    try this

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let directions= {
+  "N" : "North",
+  "S" : "South",
+  "E" : "East" ,
+  "W" : "West"
+};
+document.getElementById("result").innerHTML = directions.N;
 
+//should create a directions lookup object that looks up the four abriviations N, S, E and W and returns their corresponding full name like North
